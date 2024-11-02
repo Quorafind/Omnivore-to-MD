591769f1be1ebc5bf9c1091fecf7b9c1e3d4d3a5
quorafind <951011105@qq.com>
2024-11-02 09:30:59 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import commonjs from "vite-plugin-commonjs";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import commonjs from 'vite-plugin-commonjs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte({
-      configFile: "svelte.config.js",
-    }),
-    commonjs(),
-  ],
-});
+  plugins: [svelte(), commonjs()],
+})
